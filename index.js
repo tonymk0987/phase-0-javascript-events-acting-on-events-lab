46082ad9bf541c6095e55b1356008c053c039c95
tonymk0987 <k.tony.muriuki@gmail.com>
2024-04-01 18:58:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function moveDodgerLeft() {
+    const left = dodger.style.left.replace("px", "");
+    const leftNum = parseInt(left, 10);
+    if (leftNum > 0) {
+        dodger.style.left = `${leftNum - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const left = dodger.style.left.replace("px", "");
+    const leftNum = parseInt(left, 10);
+    if (leftNum < gameWidth - dodgerWidth) {
+        dodger.style.left = `${leftNum + 1}px`;
+    }
+}
+
+
+
